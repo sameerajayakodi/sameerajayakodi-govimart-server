b894cf695f01cd1506524201a5c86865d0ec97ee
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-07-18 23:50:09 +0530

    fix database connection error env file

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import "dotenv/config";
+
 import express from "express";
+import connectDB from "./configs/db.js";
+
 const app = express();
 const port = process.env.PORT || 4000;
-//Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
-//Middleware configuration
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("API is Working");
-});
+// Await inside top-level async function or use IIFE
+const startServer = async () => {
+  await connectDB();
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  // Allow multiple origins
+  const allowedOrigins = ["http://localhost:5173"];
+
+  // Middleware configuration
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+  // Test route
+  app.get("/", (req, res) => {
+    res.send("API is Working");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+};
+
+// Call the async function to start the server
+startServer();
