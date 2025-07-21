77c2620f33ae1e80480e61b16829388ea013a097
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-07-21 17:04:46 +0530

    fixed inStock default value

--- models/Product.js
+++ models/Product.js
@@ -8,7 +8,7 @@ const productSchema = new mongoose.Schema(
     offerPrice: { type: Number, required: true },
     image: { type: Array, required: true },
     category: { type: String, required: true },
-    inStock: { type: Boolean, default: {} },
+    inStock: { type: Boolean, default: true },
   },
   {
     timestamps: true,
