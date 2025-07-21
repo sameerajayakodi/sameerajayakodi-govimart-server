1d0d20533f46b61e976d36ba2f07ca3ac1480c09
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-07-21 23:27:52 +0530

    integrate stripe payment gateway

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,77 @@
-//place Order COD : /api/order/cod
+//place Order Stripe : /api/order/stripe
 
+import stripe from "stripe";
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
+
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, address } = req.body;
+    const { origin } = req.headers;
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid data" });
+    }
+
+    let productData = [];
+    //calculate amount
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      productData.push({
+        name: product.name,
+        price: product.price,
+        quantity: item.quantity,
+      });
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+
+    //add tax charge (2%)
+    amount += Math.floor(amount * 0.02);
+
+    const order = await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "Online",
+    });
+
+    //stripe gateway
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    //create line items for stripe
+    const line_items = productData.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: (item.price + item.price * 0.02) * 100,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    //create session
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${origin}/loader?next=my-orders`,
+      cancel_url: `${origin}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        userId,
+      },
+    });
+
+    return res.json({ success: true, url: session.url });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//place Order COD : /api/order/cod
 
 export const placeOrderCOD = async (req, res) => {
   try {
@@ -37,7 +107,8 @@ export const placeOrderCOD = async (req, res) => {
 
 export const getUserOrders = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const userId = req.userId;
+
     const orders = await Order.find({
       userId,
       $or: [{ paymentType: "COD" }, { isPaid: true }],
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,6 +3,7 @@ import {
   getAllOrders,
   getUserOrders,
   placeOrderCOD,
+  placeOrderStripe,
 } from "../controllers/orderController.js";
 import authSeller from "./../middlewares/authSeller.js";
 import authUser from "./../middlewares/authUser.js";
@@ -12,5 +13,6 @@ const orderRouter = express.Router();
 orderRouter.post("/cod", authUser, placeOrderCOD);
 orderRouter.get("/user", authUser, getUserOrders);
 orderRouter.get("/seller", authSeller, getAllOrders);
+orderRouter.post("/stripe", authSeller, placeOrderStripe);
 
 export default orderRouter;
